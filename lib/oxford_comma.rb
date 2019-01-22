07ec878ebaf74abaddc809113192d2619f440c17
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-22 02:33:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
 if array.length == 2
    array.join(" and ")
 elsif array.length >= 3
-  array.push("and")
+  array.insert (-2, "and")
   array.join(", ")
 else
   return array.join
