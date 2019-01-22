3482c54137c43d0a8586035c205d7f4e1fa66755
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-22 02:40:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,9 +3,9 @@ if array.length == 2
    array.join(" and ")
 elsif array.length >= 3
   new_array = []
+  array.join(", ")
   new_array.push(array.pop)
   array.push("and")
-  array.join(", ")
   new_array.join
   "#{array}" + "#{new_array}"
 else
